0a76bb4e4b24e6c92136527b488789c2fe8ef2a6
FTWRahul <r.poundarik@gmail.com>
2019-03-01 11:14:55 -0500

    Level 3 made and changes to prefabs

--- BlackThornProd GameJam/Assets/Scripts/RotatePlayer.cs
+++ BlackThornProd GameJam/Assets/Scripts/RotatePlayer.cs
@@ -233,4 +233,22 @@ public class RotatePlayer : MonoBehaviour {
             //    }
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Enemy"))
+        {
+            collision.gameObject.GetComponent<EnemyMove>().hitSound.Play();
+            // Kill the Enemy and destroy both Enemy and Bullet
+            //collision.GetComponent<EnemyMove>().blnDead = true;
+            collision.GetComponent<EnemyMove>().intHealth--;
+            if (collision.GetComponent<EnemyMove>().intHealth == 0)
+            {
+                collision.GetComponent<EnemyMove>().blnKilled = true;
+                //collision.gameObject.transform.rotation = Quaternion.FromToRotation(Vector3.down, collision.gameObject.transform.position - collision.gameObject.transform.position);
+                collision.GetComponent<EnemyMove>().anim.SetBool("Killed", true);
+                Destroy(collision.gameObject, gameMng.fltAnimaDestroyEnemy);
+            }
+        }
+    }
 }
