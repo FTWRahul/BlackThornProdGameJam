87fbb6efd2032714f274f11bfac1bff4bff6ecf4
Amanda <amandaduarte2012@gmail.com>
2019-02-28 23:10:03 -0500

    Setting Up the Volumes controls
    And testing it

---
+++ BlackThornProd GameJam/Assets/Scripts/Sounds.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class Sounds : MonoBehaviour
+{ 
+        private AudioSource audioSrc;
+
+        private float musicVolume = 1f;   //Music Vol. variable
+
+    public AudioMixer masterMixer;
+
+        // Use this for initialization
+        void Start()
+        {
+            audioSrc = GetComponent<AudioSource>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            audioSrc.volume = musicVolume;
+        }
+
+        // Method that is called by slider game object: This method takes vol value passed by slider and sets it as musicValue
+        public void SetVolume(float vol)
+        {
+            musicVolume = vol;
+        }
+
+    public void SetSfxLv(float sfxLvl)
+    {
+        masterMixer.SetFloat("sfxVol", sfxLvl);
+    }
+
+    public void SetMusicLv(float musicLvl)
+    {
+        masterMixer.SetFloat("musicVol", musicLvl);
+    }
+}
